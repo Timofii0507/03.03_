b45326ec17e98bddd64fac3e680fbd38b03c087a
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-12 16:34:36 +0200

    Друге  завдання  з дз

---
+++ Array2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03_
+{
+    public class Array2 : IOutput2
+    {
+        private int[] _data;
+
+        public Array2(int[] data)
+        {
+            _data = data;
+        }
+
+        public void ShowEven()
+        {
+            Console.WriteLine("Парні значення:");
+            foreach (int value in _data)
+            {
+                if (value % 2 == 0)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+
+        public void ShowOdd()
+        {
+            Console.WriteLine("Непарні значення:");
+            foreach (int value in _data)
+            {
+                if (value % 2 != 0)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Array2 array = new Array2(data);
+
+            array.ShowEven();
+            Console.WriteLine();
+            array.ShowOdd();
+        }
+    }
+
+}
