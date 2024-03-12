1e078246176ec9d119c780e49c1195fd22d8375d
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-12 14:31:51 +0200

    Перше завдання  з  дз

---
+++ Array.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03_
+{
+    public class Array : ICalc
+    {
+        private int[] _data;
+
+        public Array(int[] data)
+        {
+            _data = data;
+        }
+
+        public int Less(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int value in _data)
+            {
+                if (value < valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int Greater(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int value in _data)
+            {
+                if (value > valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._03_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            int[] data = new int[] { 1, 5, 3, 2, 4 };
+            Array array = new Array(data);
+
+            int valueToCompare = 3;
+
+            int lessCount = array.Less(valueToCompare);
+            int greaterCount = array.Greater(valueToCompare);
+
+            Console.WriteLine($"Кількість менших значень: {lessCount}");
+            Console.WriteLine($"Кількість більших значень: {greaterCount}");
+        }
+    }
+
+}
